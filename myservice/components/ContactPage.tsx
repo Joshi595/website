fb67050659efc34c7e8c90c26035d44d8680af65
Srikanth-Nalabolu <snalabolu2@gmail.com>
2026-02-03 20:55:57 +0100

    done

--- myservice/components/ContactPage.tsx
+++ myservice/components/ContactPage.tsx
@@ -52,9 +52,7 @@ const ContactPage: React.FC = () => {
     );
   }
 
-  // joshi vardhan lella
-  // srikanth git sri
-  
+
   return (
     <div className="bg-[#1a2a22] min-h-screen pt-32 pb-20">
       <div className="max-w-7xl mx-auto px-6 lg:px-12">
